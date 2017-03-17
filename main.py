121a33872cea5a3644ed5080bad874fe5116756a
David Aparicio <david.aparicio@free.fr>
2017-03-17 14:59:35 +0100

    geocoders

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import requests
-import geopy.distance
+import geopy
+
+
+def test_geocoders():
+    geolocator = geopy.geocoders.Nominatim()
+    location = geolocator.geocode("1 Place Charles Beraudier, 69003 Lyon, France")
+    # location = geolocator.geocode("175 5th Avenue NYC")
+    print(location.address)
+    print((location.latitude, location.longitude))
+
 
 def test_distances():
     partdieu = (45.760086, 4.858450)
     la__doua = (45.783560, 4.873145)
     print(geopy.distance.vincenty(partdieu, la__doua).kilometers)
 
+
 def test_requests():
     request = requests.get('https://google.com')
     print request.status_code
-    #print request.content
+    # print request.content
     return request.status_code
 
+
 def main():
-    #print "Hello, OpenSensingCity"
-    test_distances()
-    #test_requests()
+    # print "Hello, OpenSensingCity"
+    test_geocoders()
+    # test_distances()
+    # test_requests()
+
 
 if __name__ == '__main__':
     main()
--- main_test.py
+++ main_test.py
@@ -1,9 +1,11 @@
 import unittest
 import main
 
+
 class MyTest(unittest.TestCase):
     def test(self):
         self.assertEqual(main.test_requests(), 200)
+
 
 if __name__ == '__main__':
     unittest.main()
