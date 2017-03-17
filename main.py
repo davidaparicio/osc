505f5be7d7d015089a3a0d5c629867016fcaf1d1
David Aparicio <david.aparicio@free.fr>
2017-03-17 14:43:34 +0100

    add test_distances

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import requests
+import geopy.distance
+
+def test_distances():
+    partdieu = (45.760086, 4.858450)
+    la__doua = (45.783560, 4.873145)
+    print(geopy.distance.vincenty(partdieu, la__doua).kilometers)
 
 def test_requests():
     request = requests.get('https://google.com')
@@ -7,7 +13,8 @@ def test_requests():
     return request.status_code
 
 def main():
-    print "Hello, OpenSensingCity"
+    #print "Hello, OpenSensingCity"
+    test_distances()
     #test_requests()
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -5,6 +5,5 @@ class MyTest(unittest.TestCase):
     def test(self):
         self.assertEqual(main.test_requests(), 200)
 
-
 if __name__ == '__main__':
     unittest.main()
